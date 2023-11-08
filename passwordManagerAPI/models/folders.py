ca3f0bbab1031a503135b19bba6526a3f0cb0716
Sergio Guzman <sergiocamachog12@gmail.com>
2023-11-08 09:40:33 -0400

    Modify Folder model
    remove unnecesary methods that are useless

--- passwordManagerAPI/models/folders.py
+++ passwordManagerAPI/models/folders.py
@@ -3,18 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Folder(models.Model):
-    id_folder = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, null=False, blank=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    id_folder = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             null=False, blank=False, related_name='folders')
 
     def __str__(self):
         return self.name
-    
-    def get_id(self):
-        return self.id_folder
-    
-    def get_name(self):
-        return self.name
-
-    def get_user(self):
-        return self.user
