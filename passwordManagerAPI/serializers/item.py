4bcc606a58459d1d6943614f68d7c09ccff75f8b
Sergio Guzman <sergiocamachog12@gmail.com>
2023-11-08 10:39:20 -0400

    Create the item serializer

---
+++ passwordManagerAPI/serializers/item.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from models import Items
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Items
+        fields = "__all__"
+
+        extra_kwargs = {
+            'user': {'required': False},
+            'folder': {'required': False},
+            'description': {'required': False},
+            'url': {'required': False},
+        }
