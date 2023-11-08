5c39adbf8d17945157f1c847ef954b0ec6f83d72
Sergio Guzman <sergiocamachog12@gmail.com>
2023-11-08 10:21:16 -0400

    Set up the views
    Create the basic methods and classes of the views

---
+++ passwordManagerAPI/views/folders.py
@@ -0,0 +1,18 @@
+from rest_framework.views import APIView
+
+class FolderView(APIView):
+    def get(self, request):
+        pass
+    
+    def post(self, request):
+        pass
+
+class DetailFolderView(APIView):
+    def get(self, request):
+        pass
+    
+    def put(self, request):
+        pass
+    
+    def delete(self, request):
+        pass
