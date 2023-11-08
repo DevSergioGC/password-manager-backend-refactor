fd98bd118fcc083f91167893376db409cb97fd59
Sergio Guzman <sergiocamachog12@gmail.com>
2023-11-08 10:39:07 -0400

    Create the folder serializer

---
+++ passwordManagerAPI/serializers/folder.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from models import Folder
+
+class FolderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Folder
+        fields = "__all__"
+
+        extra_kwargs = {
+            'user': {'required': False}
+        }
