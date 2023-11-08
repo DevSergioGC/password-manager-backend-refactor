1cfe7d4d018ebe8aa2d85737f3fad9e30bfa81b5
Sergio Guzman <sergiocamachog12@gmail.com>
2023-11-08 09:40:58 -0400

    Modify Item Model
    remove unnecesary methods that are useless

--- passwordManagerAPI/models/items.py
+++ passwordManagerAPI/models/items.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-from folders import Folder
+from .folders import Folder
 
 
 class Items(models.Model):
     id_item = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, null=False, blank=False)
+    username = models.CharField(max_length=30, null=False, blank=False)
     password = models.CharField(max_length=255, null=False, blank=False)
     url = models.CharField(max_length=255)
     description = models.CharField(max_length=300)
@@ -13,31 +14,7 @@ class Items(models.Model):
     user = models.ForeignKey(
         User, null=False, blank=False, on_delete=models.CASCADE)
     folder = models.ForeignKey(
-        Folders, null=True, blank=True, on_delete=models.DO_NOTHING)
+        Folder, null=True, blank=True, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.name
-
-    def get_id(self):
-        return self.id_item
-
-    def get_name(self):
-        return self.name
-
-    def get_user(self):
-        return self.user
-
-    def get_folder(self):
-        return self.folder
-
-    def get_username(self):
-        return self.username
-
-    def get_password(self):
-        return self.password
-
-    def get_url(self):
-        return self.url
-
-    def get_description(self):
-        return self.description
